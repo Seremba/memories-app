0a053eae1cba34d9d544ab4bf7403bc0c6497ea9
Seremba <pserembae@gmail.com>
2021-01-27 09:28:54 +0300

    create a memory app with ionic and vue.js. Good project for learning how to use ionic and vue

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <base-layout page-title="All Memories">
+     <ion-list>
+         <ion-item 
+         v-for="memory in memories"
+         :router-link="`/memories/${memory.id}`"
+         :key="memory.id">
+        <ion-thumbnail slot="start">
+         <ion-img :src="memory.image" :atlt ="memory.title"></ion-img>
+         </ion-thumbnail>
+         <ion-label>{{memory.title}}</ion-label>
+         </ion-item>
+     </ion-list>
+    </base-layout>
+</template>
+
+<script>
+import { IonList,IonItem, IonImg, IonThumbnail,IonLabel } from '@ionic/vue'
+import BaseLayout from '../components/base/BaseLayout.vue'
+
+export default {
+    components: {
+        IonItem,
+        IonList,
+        BaseLayout,
+        IonImg,
+        IonThumbnail,
+        IonLabel
+    },
+   computed: {
+       memories() {
+           return this.$store.getters.memories;
+       }
+   }
+    
+}
+</script>  
---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import {createStore} from 'vuex';
+
+const store = createStore({
+    state(){
+        return {
+            memories: [
+                {
+                    id: 'm1',
+                    title: 'A trip into the mountains',
+                    image: 'https://images.unsplash.com/photo-1486870591958-9b9d0d1dda99?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
+                    description: 'what a wonderful moment'  
+                },
+                {
+                    id: 'm2',
+                    title: 'Swimming in the lake',
+                    image: 'https://images.unsplash.com/photo-1438029071396-1e831a7fa6d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1292&q=80',
+                    description: 'this is so cool'
+                },
+                {
+                    id: 'm3',
+                    title: 'Sex in the Camp',
+                    image: 'https://images.unsplash.com/photo-1524706088418-1b1ce0a23b20?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1188&q=80',
+                    description: 'Bino byanyumye luno hoo'
+                }
+            ]
+        }
+
+    },
+    getters: {
+        memories(state){
+            return state.memories
+        },
+        memory(state) {
+           return (memoryId) => {
+               return state.memories.find((memory) => memory.id === memoryId)
+           };
+        }
+    }
+});
+
+export default store;
